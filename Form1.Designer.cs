a39aa45f6e8d50dabe5d5f979ae13f9a0117e648
Mac Work <maciox55@gmail.com>
2022-08-29 16:07:20 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace PCDMapper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnTextBox = new System.Windows.Forms.TextBox();
+            this.pnLabel = new System.Windows.Forms.Label();
+            this.snLabel = new System.Windows.Forms.Label();
+            this.snTextBox = new System.Windows.Forms.TextBox();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // pnTextBox
+            // 
+            this.pnTextBox.Location = new System.Drawing.Point(11, 70);
+            this.pnTextBox.Name = "pnTextBox";
+            this.pnTextBox.Size = new System.Drawing.Size(177, 20);
+            this.pnTextBox.TabIndex = 0;
+            // 
+            // pnLabel
+            // 
+            this.pnLabel.AutoSize = true;
+            this.pnLabel.Location = new System.Drawing.Point(11, 54);
+            this.pnLabel.Name = "pnLabel";
+            this.pnLabel.Size = new System.Drawing.Size(66, 13);
+            this.pnLabel.TabIndex = 1;
+            this.pnLabel.Text = "Part Number";
+            // 
+            // snLabel
+            // 
+            this.snLabel.AutoSize = true;
+            this.snLabel.Location = new System.Drawing.Point(191, 54);
+            this.snLabel.Name = "snLabel";
+            this.snLabel.Size = new System.Drawing.Size(73, 13);
+            this.snLabel.TabIndex = 3;
+            this.snLabel.Text = "Serial Number";
+            // 
+            // snTextBox
+            // 
+            this.snTextBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.snTextBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.snTextBox.Location = new System.Drawing.Point(194, 70);
+            this.snTextBox.Name = "snTextBox";
+            this.snTextBox.Size = new System.Drawing.Size(146, 20);
+            this.snTextBox.TabIndex = 2;
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(355, 68);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(75, 23);
+            this.startBtn.TabIndex = 4;
+            this.startBtn.Text = "Start PCD";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            this.startBtn.KeyDown += new System.Windows.Forms.KeyEventHandler(this.startBtn_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Location = new System.Drawing.Point(8, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(422, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Enter Part Number and Serial Number of the machine you are about to run PCDoctor " +
+    "on.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(448, 100);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.snLabel);
+            this.Controls.Add(this.snTextBox);
+            this.Controls.Add(this.pnLabel);
+            this.Controls.Add(this.pnTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "PCD Mapper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox pnTextBox;
+        private System.Windows.Forms.Label pnLabel;
+        private System.Windows.Forms.Label snLabel;
+        private System.Windows.Forms.TextBox snTextBox;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
