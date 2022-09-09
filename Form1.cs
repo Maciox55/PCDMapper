b847662c49ec7af324dd57dc47e8dd540dada91a
Mac Work <maciox55@gmail.com>
2022-09-09 15:36:33 -0400

    Fixed args with name, serial and part number

--- Form1.cs
+++ Form1.cs
@@ -49,9 +49,13 @@ namespace PCDMapper
         {
             if (pnTextBox.Text != String.Empty && snTextBox.Text != String.Empty && matched)
             {
-                //string args = config.GetBaseParams() + " -id " + snTextBox.Text + " -g " + matchedBinding.groupp + " -c " + matchedBinding.configuration + " -p " + matchedBinding.phase + " -e \"TESTER="+testerName.Text + ",SERIAL_NUMBER=" + snTextBox.Text + ",PART_NUMBER="+pnTextBox.Text+ "\"";
-                
-                string args = config.GetBaseParams() + " -id " + snTextBox.Text + " -g " + matchedBinding.groupp + " -c " + matchedBinding.configuration + " -p " + matchedBinding.phase;
+                //string args = config.GetBaseParams() + " -id " + snTextBox.Text + " -g " + matchedBinding.groupp + " -c " + matchedBinding.configuration + " -p " + matchedBinding.phase + " -e \"TESTER=" + testerName.Text + ",SERIAL_NUMBER=" + snTextBox.Text + ",PART_NUMBER=" + pnTextBox.Text + "\"";
+
+                string extraData=" -e \"TESTER=" + testerName.Text + ",SERIAL_NUMBER=" + snTextBox.Text + ",PART_NUMBER=" + pnTextBox.Text + "\"";
+
+                string args = config.GetBaseParams() + " -id " + snTextBox.Text + " -g " + matchedBinding.groupp + " -c " + matchedBinding.configuration + " -p " + matchedBinding.phase + extraData;
+
+                //string args = config.GetBaseParams() + " -id " + snTextBox.Text + " -g " + matchedBinding.groupp + " -c " + matchedBinding.configuration + " -p " + matchedBinding.phase;
 
                 //MessageBox.Show(args);
                 CMDR.GetProcess().RunCommand(config.GetPath(), @"/k pcdgui.exe " + args);
